f95ec2f5e389c6e5440a59bc7f533c91c4391f3f
Simone Belia <simone.belia@gmail.com>
2022-08-09 20:04:04 +0200

    some model placeholder

---
+++ src/Atlas.Core/AtlasManagementClient.cs
@@ -0,0 +1,21 @@
+﻿using Atlas.Core.Configuration;
+using RestSharp;
+using RestSharp.Serializers.NewtonsoftJson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atlas.Core
+{
+    public class AtlasManagementClient : ClientBase, IAtlasManagementClient
+    {
+        public AtlasManagementClient(AtlasOptions options)
+        {
+            _options = options;
+            _http = new RestClient(options.BaseUrl);
+            _http.UseNewtonsoftJson(options.SerializerOptions);
+        }
+    }
+}
