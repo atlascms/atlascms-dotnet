de03e707cf127dd03b6b9be48b0c30d7ea5f2d37
Simone Belia <simone.belia@gmail.com>
2022-08-13 12:04:03 +0200

    Setup Test Project

---
+++ src/Atlas.Core.Tests/AtlasClientTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Atlas.Core.Tests
+{
+    public class AtlasClientTests
+    {
+        [Fact]
+        public void ShouldWork()
+        {           
+            Assert.Equal(1,1);
+        }
+    }
+}
